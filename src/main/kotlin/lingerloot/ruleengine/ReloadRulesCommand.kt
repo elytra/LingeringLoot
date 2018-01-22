437cdc609efcd56b836fb89073bcaaade59549f0
Alex Ponebshek <alexbobp@gmail.com>
2018-01-21 23:49:05 -0600

    no perm checking in command, annd rename to llreload

--- src/main/kotlin/lingerloot/ruleengine/ReloadRulesCommand.kt
+++ src/main/kotlin/lingerloot/ruleengine/ReloadRulesCommand.kt
@@ -10,17 +10,9 @@ import net.minecraft.server.dedicated.DedicatedServer
 import net.minecraft.util.text.TextComponentString
 
 object ReloadRulesCommand: CommandBase() {
-    override fun getName() = "reloadll"
-    override fun getUsage(sender: ICommandSender?) = "/reloadll"
+    override fun getName() = "llreload"
+    override fun getUsage(sender: ICommandSender?) = "/llreload"
 
-    override fun execute(server: MinecraftServer?, sender: ICommandSender?, args: Array<out String>?) = sender?.let{
-        if (when (it) {
-            is EntityPlayerMP -> {server!!.playerList.oppedPlayers.getEntry(it.gameProfile)?.permissionLevel?:0 >= server.opPermissionLevel}
-            is RConConsoleSource -> true
-            is DedicatedServer -> true
-            else -> false
-        }) {
-            it.sendMessage(TextComponentString(cfg!!.reloadRules().map({it}, {it})))
-        }
-    }?:Unit
+    override fun execute(server: MinecraftServer?, sender: ICommandSender?, args: Array<out String>?) =
+            sender?.sendMessage(TextComponentString(cfg!!.reloadRules().map({it}, {it})))?:Unit
 }
