3da82a8dfba4928e435a3b26bccee0b3033b0d72
Alex Ponebshek <alexbobp@gmail.com>
2018-01-31 07:43:25 -0600

    add kotlin example

--- src/main/kotlin/com/elytradev/concrete/rulesengine/RulesEngineJava.kt
+++ src/main/kotlin/com/elytradev/concrete/rulesengine/RulesEngineJava.kt
@@ -6,7 +6,7 @@ import java.util.function.Predicate
 
 abstract class RulesEngineJava<X : EvaluationContext> : RulesEngine<X>() {
     override fun getDomainPredicates(): Map<Char, Function1<String, Either<Predicate<X>, String>>> {
-        return jDomainPredicates().mapValues { it.value::apply }
+        return jDomainPredicates().mapValues { it.value::apply } // yes intellij very suspicious
     }
 
     abstract fun jDomainPredicates(): Map<Char, Function<String, Either<Predicate<X>, String>>>
